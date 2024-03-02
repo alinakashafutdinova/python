6f3be45b56104e9813abb41a5d04ae24f8c4c78e
alinakashafutdinova <150508559+alinakashafutdinova@users.noreply.github.com>
2024-03-02 11:17:00 +0300

    Add files via upload

---
+++ lab006/main.py
@@ -0,0 +1,7 @@
+import turtle
+turtle.shape("turtle")
+turtle.speed(2)
+turtle.color("red")
+for i in range(18):
+ turtle.forward(150)
+ turtle.left(100)
---
+++ lab006/main2.py
@@ -0,0 +1,7 @@
+import turtle
+turtle.shape("turtle")
+turtle.speed(100)
+turtle.color("black")
+for i in range(50):
+ turtle.circle(3*i)
+ turtle.left(60)
