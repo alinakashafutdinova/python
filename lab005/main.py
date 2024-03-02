01f8f2ed77b313403eb36c0bbb9ff530723f5b0d
alinakashafutdinova <150508559+alinakashafutdinova@users.noreply.github.com>
2024-03-02 10:59:42 +0300

    Add files via upload

---
+++ lab005/main.py
@@ -0,0 +1,16 @@
+import turtle
+turtle.shape("turtle")
+turtle.color("green")
+turtle.speed(2)
+size = 400
+turtle.circle(40)
+turtle.right(180)
+turtle.circle(60)
+turtle.penup()
+
+
+turtle.left(90)
+turtle.forward(120)
+turtle.right(90)
+turtle.pendown()
+turtle.circle(80)
