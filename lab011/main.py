40921c2af95743dc47c1f1b38ac6a946fba24a23
Name <johndoe@example.com>
2024-04-06 10:32:21 +0300

    11

--- lab011/main.py
+++ lab011/main.py
@@ -1,1 +1,49 @@
-ghbfdhyk,hryr
+from turtle import *
+from random import randint
+shape("turtle")
+colors = ["blue", "red", "orange", "purple"]
+shredder = randint(1,10)
+user_choose = 0
+while user_choose != shredder:
+ user_choose = str.lower(input("select
+ direction Left/Right/Direct/Back - "))
+22
+Урок 11
+ if user_choose == str.lower("left"):
+ left(90)
+ for color_turtle in colors:
+ pensize(randint(1,10))
+ color(color_turtle)
+ forward(randint(5,30))
+ user_choose = randint(1,10)
+ if user_choose!= shredder:
+ print("Shredder is not here, let's move on")
+ elif user_choose == str.lower("right"):
+ right(90)
+ for color_turtle in colors:
+ pensize(randint(1,10))
+ color(color_turtle)
+ forward(randint(5,30))
+ user_choose = randint(1,10)
+ if user_choose!= shredder:
+ print("Shredder is not here, let's move on")
+ elif user_choose == str.lower("direct"):
+ for color_turtle in colors:
+ pensize(randint(1,10))
+ color(color_turtle)
+ forward(randint(5,30))
+ user_choose = randint(1,10)
+ if user_choose!= shredder:
+ print("Shredder is not here, let's move on")
+ elif user_choose == str.lower("back"):
+23
+Цикл for и функция range. Игра «Черепашки-ниндзя»
+ left(180)
+ for color_turtle in colors:
+ pensize(randint(1,10))
+ color(color_turtle)
+ forward(randint(5,30))
+ user_choose = randint(1,10)
+ if user_choose!= shredder:
+ print("Shredder is not here, let's move on")
+print("Shredder here!")
