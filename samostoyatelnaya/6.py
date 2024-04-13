ba3e2af1b5d80f09c02e82732e269dbefca5bf43
Name <johndoe@example.com>
2024-04-13 10:06:08 +0300

    add

--- samostoyatelnaya/6.py
+++ samostoyatelnaya/6.py
@@ -1,22 +1,25 @@
 import turtle
 
-# Запрашиваем радиус первого круга у пользователя
 radius = float(input("Введите радиус первого круга (синего): "))
 
-# Создаем экземпляр черепахи
 t = turtle.Turtle()
 
 # Рисуем синий круг с указанным радиусом
 t.color("blue")
+t.penup()
+t.goto(0, 0)
+t.pendown()
 t.circle(radius)
 
-# Рисуем остальные круги только с контурами
+colors = ["red", "green", "orange", "purple"]
+
+# Рисуем остальные круги с разными цветами
 for i in range(1, 5):
     t.penup()
-    t.goto(0, 0)
+    t.goto(0, -radius*(i+0.5))
     t.pendown()
-    t.color("red")
+    t.color(colors[i-1])
     t.circle(radius * (i+1))
 
-# Завершаем работу черепахи
 turtle.done()
+
