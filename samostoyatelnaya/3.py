68ae6c10be1cfca5a89b340a2e17b573be6efd31
Name <johndoe@example.com>
2024-04-13 11:37:29 +0300

    add 3

--- samostoyatelnaya/3.py
+++ samostoyatelnaya/3.py
@@ -1,8 +1,9 @@
 massa = float(input("Введи массу конфет в граммах: ")) 
 
 if massa > 2000: 
-    chena = massa / 1000 * 200 
-    print(f"200 руб за 1 кг, Итого: {chena} руб") 
+    price = massa / 1000 * 200 
+    print(f"200 руб за 1 кг| Итого: {price} руб") 
 else: 
-    chena = massa / 1000 * 250 
-    print(f"250 руб за 1 кг, Итого: {chena} руб")
+    price = massa / 1000 * 250 
+    print(f"250 руб за 1 кг| Итого: {price} руб")
+
